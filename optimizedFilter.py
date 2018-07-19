591a0bae3dd29fda3c7a10ad205de8a4eb1b493f
Dale Erikson <dje23@pitt.edu>
2018-07-18 23:58:34 -0400

    Adding filter

---
+++ optimizedFilter.py
@@ -0,0 +1,19 @@
+from __future__ import print_function
+# import rdkit components
+from rdkit import rdBase
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import Draw
+from rdkit.Chem import rdMolDescriptors
+from rdkit.Chem import Descriptors
+from rdkit.Chem import rdchem
+import sys
+from sys import argv as arg
+from rdkit.Chem.Fingerprints import FingerprintMols
+from rdkit import DataStructs
+
+leadFile = open(arg[1],"r")
+for line in leadFile
+    tempMol = Chem.MolFromSmiles(line.strip("\n"))
+    if(Descriptors.MolWt(tempMol, 0) < 500)
+        print(Chem.MolToSmiles(tempMol))
